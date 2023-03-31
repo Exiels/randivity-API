81254c0c481a67d57879390d956b3ccecc517b67
Exiels <Samina29@outlook.fr>
2023-03-31 03:57:35 +0200

    [REM] Removed tmp files test

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,13 +5,9 @@
 const express = require('express')
 const router = express.Router()
 
-const test = require('./test/tmp')
-
 /**
  * Main router connection
  * @namespace mainRouter
  */
-
-router.use('/test', test)
 
 module.exports = router
--- src/routes/test/tmp.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  try {
-    return res.status(200).json({ message: "ok"})
-  } catch (error) {
-    console.error(error)
-    return res.status(500).json({ message: 'Internal Server Error' })
-  }
-}
