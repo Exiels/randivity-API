65e86a517810021959a55f71edb781112a5f4093
Exiels <Exiels@users.noreply.github.com>
2023-04-02 01:37:40 +0000

    [LINT] Applied linter fix

--- src/__tests__/routes/user.test.js
+++ src/__tests__/routes/user.test.js
@@ -6,149 +6,149 @@ const dbDefault = require('../../config/db.default')
 const { Users } = require('../../models/user')
 
 describe('User route tests', () => {
-    let app
+  let app
 
-    beforeAll(async () => {
-      app = await server.testServer()
+  beforeAll(async () => {
+    app = await server.testServer()
+  })
+
+  afterEach(async () => {
+    const collections = mongoose.connection.collections
+    for (const key in collections) {
+      const collection = collections[key]
+      collection.deleteMany()
+    }
+    await dbDefault()
+  })
+
+  afterAll(async () => {
+    await mongoose.connection.close()
+  })
+
+  describe('Login route', () => {
+    it('POST /user/login => Try good login', async () => {
+      return await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
     })
 
-    afterEach(async () => {
-        const collections = mongoose.connection.collections
-        for (const key in collections) {
-            const collection = collections[key]
-            collection.deleteMany()
-        }
-        await dbDefault()
+    it('POST /user/login => Try bad username', async () => {
+      return await request(app)
+        .post('/user/login')
+        .send({
+          username: 'test',
+          password: 'admin123'
+        })
+        .expect(401)
     })
 
-    afterAll(async () => {
-      await mongoose.connection.close()
+    it('POST /user/login => Try bad password', async () => {
+      return await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'test'
+        })
+        .expect(401)
     })
 
-    describe('Login route', () => {
-        it('POST /user/login => Try good login', async () => {
-        return await request(app)
-            .post('/user/login')
-            .send({
-                username: 'admin',
-                password: 'admin123'
-            })
-            .expect('Content-Type', /json/)
-            .expect(200)
+    it('POST /user/login => Try bad form', async () => {
+      return await request(app)
+        .post('/user/login')
+        .send({
+          username: 'test'
+        })
+        .expect(401)
+    })
+  })
+
+  describe('ChangePassword route', () => {
+    it('POST /user/password => Try change password', async () => {
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
         })
 
-        it('POST /user/login => Try bad username', async () => {
-            return await request(app)
-            .post('/user/login')
-            .send({
-                username: 'test',
-                password: 'admin123'
-            })
-            .expect(401)
+      await request(app)
+        .patch('/user/password')
+        .set({
+          'x-auth-token': key
         })
+        .send({
+          password: 'test'
+        })
+        .expect(200)
 
-        it('POST /user/login => Try bad password', async () => {
-            return await request(app)
-            .post('/user/login')
-            .send({
-                username: 'admin',
-                password: 'test'
-            })
-            .expect(401)
+      return await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'test'
         })
-
-        it('POST /user/login => Try bad form', async () => {
-            return await request(app)
-            .post('/user/login')
-            .send({
-                username: 'test'
-            })
-            .expect(401)
-        })
+        .expect(200)
     })
 
-    describe('ChangePassword route', () => {
-        it('POST /user/password => Try change password', async () => {
-            let key
-        
-            await request(app)
-            .post('/user/login')
-            .send({
-                username: 'admin',
-                password: 'admin123'
-            })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((response) => {
-                key = response.body.token
-            })
-
-            await request(app)
-            .patch('/user/password')
-            .set({
-                'x-auth-token': key
-            })
-            .send({
-                password: 'test'
-            })
-            .expect(200)
-
-            return await request(app)
-            .post('/user/login')
-            .send({
-                username: 'admin',
-                password: 'test'
-            })
-            .expect(200)
+    it('POST /user/password => Try bad token', async () => {
+      return await request(app)
+        .patch('/user/password')
+        .set({
+          'x-auth-token': 'test'
         })
-
-        it('POST /user/password => Try bad token', async () => {
-            return await request(app)
-            .patch('/user/password')
-            .set({
-                'x-auth-token': "test"
-            })
-            .send({
-                password: 'test'
-            })
-            .expect(400)
+        .send({
+          password: 'test'
         })
-
-        it('POST /user/password => Try No token', async () => {
-            return await request(app)
-            .patch('/user/password')
-            .send({
-                password: 'test'
-            })
-            .expect(403)
-        })
-
-        it('POST /user/password => Try if user not exist', async () => {
-            let key
-        
-            await request(app)
-            .post('/user/login')
-            .send({
-                username: 'admin',
-                password: 'admin123'
-            })
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((response) => {
-                key = response.body.token
-            })
-
-            await Users.findOneAndRemove({ username: 'admin' })
-
-            return await request(app)
-            .patch('/user/password')
-            .set({
-                'x-auth-token': key
-            })
-            .send({
-                password: 'test'
-            })
-            .expect(500)
-        })
+        .expect(400)
     })
+
+    it('POST /user/password => Try No token', async () => {
+      return await request(app)
+        .patch('/user/password')
+        .send({
+          password: 'test'
+        })
+        .expect(403)
+    })
+
+    it('POST /user/password => Try if user not exist', async () => {
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+
+      await Users.findOneAndRemove({ username: 'admin' })
+
+      return await request(app)
+        .patch('/user/password')
+        .set({
+          'x-auth-token': key
+        })
+        .send({
+          password: 'test'
+        })
+        .expect(500)
+    })
+  })
 })
--- src/config/db.default.js
+++ src/config/db.default.js
@@ -2,7 +2,7 @@ const { Users } = require('../models/user')
 const bcrypt = require('bcryptjs')
 
 async function initDefaultUsers () {
-  const tmp = await Users.find({ username: 'admin'})
+  const tmp = await Users.find({ username: 'admin' })
 
   // We check if the db is empty and if it needs to be initialized
   if (tmp === undefined || tmp === null || tmp.length === 0) {
