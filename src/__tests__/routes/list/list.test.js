dde8164fa2294a1bf0c53704f301f25497b9e86e
Exiels <Exiels@users.noreply.github.com>
2023-04-05 03:59:31 +0000

    [LINT] Applied linter fix

--- src/__tests__/routes/list/list.test.js
+++ src/__tests__/routes/list/list.test.js
@@ -26,253 +26,253 @@ describe('list routes tests', () => {
 
   describe('Lists route', () => {
     it('GET /list => Get list of lists', async () => {
-        let code
-        let key
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .get('/list')
-          .set({
-            'server-code': code
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
+      let code
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .get('/list')
+        .set({
+          'server-code': code
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
     })
 
     it('POST /list => Create a new valid list', async () => {
-        let code
-        let key
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .post('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'name': 'test'
-          })
-          .expect('Content-Type', /json/)
-          .expect(201)
+      let code
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .post('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          name: 'test'
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
     })
 
     it('POST /list => Create a new invalid list', async () => {
-        let code
-        let key
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .post('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'name': 'test'
-          })
-          .expect('Content-Type', /json/)
-          .expect(201)
-          await request(app)
-          .post('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'name': 'test'
-          })
-          .expect('Content-Type', /json/)
-          .expect(409)
+      let code
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .post('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          name: 'test'
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
+      await request(app)
+        .post('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          name: 'test'
+        })
+        .expect('Content-Type', /json/)
+        .expect(409)
     })
 
     it('DELETE /list => Delete a list', async () => {
-        let code
-        let key
-        let id
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .post('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'name': 'test'
-          })
-          .expect('Content-Type', /json/)
-          .expect(201)
-          await request(app)
-          .get('/list')
-          .set({
-            'server-code': code
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            id = response.body[0]._id
-          })
-          await request(app)
-          .delete('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'id': id
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
+      let code
+      let key
+      let id
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .post('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          name: 'test'
+        })
+        .expect('Content-Type', /json/)
+        .expect(201)
+      await request(app)
+        .get('/list')
+        .set({
+          'server-code': code
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          id = response.body[0]._id
+        })
+      await request(app)
+        .delete('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          id
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
     })
 
     it('DELETE /list => Delete invalid list invalid ObjectID', async () => {
-        let code
-        let key
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .delete('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'id': 'test'
-          })
-          .expect('Content-Type', /json/)
-          .expect(400)
+      let code
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .delete('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          id: 'test'
+        })
+        .expect('Content-Type', /json/)
+        .expect(400)
     })
 
     it('DELETE /list => Delete invalid list', async () => {
-        let code
-        let key
-  
-        await request(app)
-          .post('/user/login')
-          .send({
-            username: 'admin',
-            password: 'admin123'
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            key = response.body.token
-          })
-        await request(app)
-          .post('/admin/code')
-          .set({
-            'x-auth-token': key
-          })
-          .expect('Content-Type', /json/)
-          .expect(200)
-          .then((response) => {
-            code = response.body.code
-          })
-          await request(app)
-          .delete('/list')
-          .set({
-            'server-code': code
-          })
-          .send({
-            'id': '642cae58db194f6ca6f38ab3'
-          })
-          .expect('Content-Type', /json/)
-          .expect(404)
+      let code
+      let key
+
+      await request(app)
+        .post('/user/login')
+        .send({
+          username: 'admin',
+          password: 'admin123'
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          key = response.body.token
+        })
+      await request(app)
+        .post('/admin/code')
+        .set({
+          'x-auth-token': key
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          code = response.body.code
+        })
+      await request(app)
+        .delete('/list')
+        .set({
+          'server-code': code
+        })
+        .send({
+          id: '642cae58db194f6ca6f38ab3'
+        })
+        .expect('Content-Type', /json/)
+        .expect(404)
     })
   })
 })
