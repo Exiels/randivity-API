35e237ce3d33e36699e7f15f0b181cddba5c23cd
Exiels <Samina29@outlook.fr>
2023-04-05 06:00:24 +0200

    [REM] Removed unused var

--- src/routes/list/router.js
+++ src/routes/list/router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const auth = require('../../middleware/auth')
 const code = require('../../middleware/code')
 
 const list = require('./list')
