3756c3c9ed5b668d47309ba45676756359fbeb0e
Exiels <Samina29@outlook.fr>
2023-04-05 08:01:17 +0200

    [ADD]Added sanitizer to testServer

--- src/__tests__/serverUtils/testServer.js
+++ src/__tests__/serverUtils/testServer.js
@@ -1,15 +1,16 @@
 const express = require('express')
 
+const app = express()
 const router = require('../../routes/router')
 const { dbConnection } = require('../../config/db')
+const sanitizer = require('../../middleware/sanitize')
 
 async function testServer () {
   await dbConnection('test')
 
-  const app = express()
   app.use(express.json())
 
-  app.use('/', router)
+  app.use('/', sanitizer, router)
 
   return app
 }
